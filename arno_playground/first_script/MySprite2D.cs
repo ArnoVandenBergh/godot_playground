5b7a63901e3064bb9dea0172daac0d0944e683de
Arno Van den Bergh <arnovdb12345@gmail.com>
2024-11-15 21:50:15 +0100

    update that is not working

--- arno_playground/first_script/MySprite2D.cs
+++ arno_playground/first_script/MySprite2D.cs
@@ -12,24 +12,13 @@ public partial class MySprite2D : Sprite2D
 
     public override void _Process(double delta)
     {
-        var velocity = Vector2.Zero;
-        if (Input.IsActionPressed("ui_up"))
-        {
-            velocity = Vector2.Up.Rotated(Rotation) * _speed;
-        }
-
+        Rotation += _angularSpeed * (float)delta;
+        var velocity = Vector2.Up.Rotated(Rotation) * _speed;
         Position += velocity * (float)delta;
+    }
 
-        var direction = 0;
-        if (Input.IsActionPressed("ui_left"))
-        {
-            direction = -1;
-        }
-        if (Input.IsActionPressed("ui_right"))
-        {
-            direction = 1;
-        }
-
-        Rotation += _angularSpeed * direction * (float)delta;
+    private void OnButtonPressed()
+    {
+        SetProcess(!IsProcessing());
     }
 }
