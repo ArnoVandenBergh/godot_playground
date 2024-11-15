0b0a1568a43e4974c96c69037438c978388cb9ce
Arno Van den Bergh <arnovdb12345@gmail.com>
2024-11-15 20:57:29 +0100

    Add moving rotation

--- arno_playground/first_script/MySprite2D.cs
+++ arno_playground/first_script/MySprite2D.cs
@@ -3,8 +3,18 @@ using System;
 
 public partial class MySprite2D : Sprite2D
 {
+    private int _speed = 400;
+    private float _angularSpeed = Mathf.Pi;
     public MySprite2D()
     {
         GD.Print("Hello, world!");
+    }
+
+    public override void _Process(double delta)
+    {
+        Rotation += _angularSpeed * (float)delta;
+        var velocity = Vector2.Up.Rotated(Rotation) * _speed;
+
+        Position += velocity * (float)delta;
     }
 }
